2578c337bca342d5970a63957d124a541b65fc84
allenvo200 <allenvo200@gmail.com>
2024-04-22 03:40:38 -0600

    allens branch!

--- library_app.py
+++ library_app.py
@@ -29,10 +29,28 @@ def print_menu():
     print("3. Return a book")
     print("0. Exit the system")
     selection = int(input("Enter your selection: "))
-    while selection > (3):
+    print(selection)
+    while selection > (3) and selection != 2130:
         print("Invalid option")
         selection = int(input("Enter your selection: "))
     return selection
+
+def print_menu_librian():
+    print("\nReader's Guild Library - Librian Menu")
+    print("==================================")
+    print("1. Search for books")
+    print("2. Borrow a book")
+    print("3. Return a book")
+    print("4. Add a book")
+    print("5. Remove a book")
+    print("6. Update a book")
+    print("0. Exit the system")
+    selection = int(input("Enter your selection: "))
+    while selection > (6):
+        print("Invalid option")
+        selection = int(input("Enter your selection: "))
+    return selection
+
 
 #searches for the book in the list and returns the book if found(searches per index), converts everything is in lowercase to scrape data easier
 def check_book(book_list):
@@ -45,6 +63,7 @@ def check_book(book_list):
         for field in current_book.get_all_info():
             if search_value.lower() in str(field).lower():
                 print("Found a match... :", current_book.__str__()) #This is the print book function __str__ from book.py
+                return
         index += 1
 
 #borrow book function, searches for the book by ISBN, if found, borrow the book, if not found, print no book found
@@ -60,6 +79,7 @@ def borrow_book(book_list):
             return
 
         index += 1 
+
     print("No book found with that ISBN")  
 
 #return book function, searches for the book by ISBN, if found, return the book, if not found, print no book found
@@ -74,20 +94,79 @@ def return_book(book_list):
             return
 
         index += 1 
+    
     print("No book found with that ISBN")  
 
+#ADDS THE BOOKS TOGETHER!
+def add_book(book_list):
+    print("-- Add a book --")
+    isbn = str(input("Enter the 13 digit ISBN (format 999-9999999999) "))
+    title = str(input("Enter the title of the book: "))
+    author = str(input("Enter the author of the book: "))
+    genre = int(input("Enter the genre of the book (0-9): "))
+    availability = True
+    new_book = book.Book(isbn, title, author, genre, availability)
+    book_list.append(new_book)
+    print("Book has been added to the catalog")
 
+#REMOVE THE BOOKS FROM THE LIST~!
+def remove_book(book_list):
+    print("-- Remove a book --")
+    find_book_by_isbn = str(input("Enter the 13 digit ISBN (format 999-9999999999) "))
+    index = 0 
+    while index < len(book_list):
+        current_book = book_list[index]
+        if find_book_by_isbn == str(current_book.get_isbn()):
+            book_list.remove(current_book)
+            print("Book has been removed from the catalog")
+            return
+
+        index += 1 
+    
+    print("No book found with that ISBN")
+
+#PRINTS ALL THE BOOKS IN THE LIST~! checks the list and prints out the books in the list
+def print_book(book_list):
+    print("\n-- Book Catalog --")
+    print("{:14s} {:25s} {:25s} {:20s} {:s}".format("ISBN", "Title",
+        "Author", "Genre", "Availability"))
+    for b in book_list:
+        print(b)
 
 def main():
     book_list = load_books()
 
-    selection = print_menu()
-   
-    if selection == 1:
-        check_book(book_list)
-    elif selection == 2:
-        borrow_book(book_list)
-    elif selection == 3:
-        return_book(book_list)
-    
+
+    while True:
+        selection = print_menu()
+        if selection == 1:
+            check_book(book_list)
+        elif selection == 2:
+            borrow_book(book_list)
+        elif selection == 3:
+            return_book(book_list)
+        elif selection == 0:
+            print("Exiting the system ...")
+            return
+        elif selection == 2130:
+            selection = print_menu_librian()
+
+            if selection == 1:
+                check_book(book_list)
+            elif selection == 2:
+                borrow_book(book_list)
+            elif selection == 3:
+                return_book(book_list)
+            elif selection == 4:
+                add_book(book_list)
+            elif selection == 5:
+                remove_book(book_list)
+            elif selection == 6:
+                print_book(book_list)
+            elif selection == 0:
+                print("Exiting the system ...")
+                return
+        
+      
+
 main()    
